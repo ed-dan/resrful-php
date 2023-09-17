c68504168790cdde0171e1e7c47fcacdbab7626d
ed-Dan <danil.tverdokhlib@nure.ua>
2023-09-17 23:11:01 +0300

    restful api on pure php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register(function ($class){
+    require __DIR__ . "/src/$class.php";
+});
+
+set_exception_handler("ErrorHandler::handleException");
+
+header("Content-type: application/json; charset=UTF-8 ");
+
+$parts = explode("/",$_SERVER["REQUEST_URI"]);
+
+if ($parts[1] != "products"){
+    http_response_code(404);
+    exit;
+}
+
+$id = $parts[2] ?? null;
+
+$database = new DataBase("localhost", "products", "root", "");
+
+$gateway = new ProductGateway($database);
+
+$controller = new ProductController($gateway);
+$controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
